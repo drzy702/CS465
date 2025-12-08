6acfea66dc205b3d920f53540f5a530dbf639e80
Quang Drazy Nguyen <drzy.ngu@gmail.com>
2025-12-07 22:44:16 -0800

    Module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express"); // Express app
+const router = express.Router(); // Router logic
+
+const tripsController = require("../controllers/trips");
+
+// define route for our trips endpoint
+router
+    .route("/trips")
+    .get(tripsController.tripsList);
+
+// GET method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
+
+module.exports = router;
